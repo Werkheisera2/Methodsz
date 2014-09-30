a84322d25c6107344084d8863573ef5f5e27b801
Werkheisera2 <werkheisera2@gmail.com>
2014-09-30 08:39:43 -0400

    $$$

---
+++ Methods/InchesToFeet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet
+{
+    class Program
+    {
+        public static void DisplayFeetAndInches(int inches)
+        {
+            Console.WriteLine(inches / 12 + " feet, and " + inches % 12 +" inches.");
+        }
+
+        static void Main()
+        {
+            int inches = 75;
+
+            DisplayFeetAndInches(inches);
+
+            
+        }
+        public static void InchesToFeet 
+        
+    
+    
+    }
+    
+}
+
+
